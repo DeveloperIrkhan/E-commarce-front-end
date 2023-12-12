123e413ef76ab44424ac3330858632bd06f7668e
irfan shah <programmer.irfansha@hotmail.com>
2023-12-12 11:00:01 +0500

    api end points created

--- e-commarce/src/API_ENDPOINTS/API_endPoints.js
+++ e-commarce/src/API_ENDPOINTS/API_endPoints.js
@@ -1,6 +1,20 @@
-export const api_endpoint="http://localhost:4040"
-export const endpointforRegister="/auth/register"
-export const endpointforLogin="/auth/Login"
-export const dashboard="/auth/user-dashboard"
-export const admin_dashboard="/auth/admin-dashboard"
-export const endPointForForgotPassword="/auth/forgot-password"
+export const api_endpoint = "http://localhost:4040";
+export const endpointforRegister = "/auth/register";
+export const endpointforLogin = "/auth/Login";
+export const dashboard = "/auth/user-dashboard";
+export const admin_dashboard = "/auth/admin-dashboard";
+export const endPointForForgotPassword = "/auth/forgot-password";
+
+//category
+export const createCategory = "/category/create-category";
+export const updateCategory = "/category/update-category/:id";
+export const getAllCategories = "/category/categories";
+export const getSingleCategory = "/category/get-category/:slug";
+export const deleteCategory = "/category/delete-category/:id";
+//products
+export const createProduct="/product/create-products"
+export const updateProduct="/product/update-product/:pid"
+export const getAllProducts="/product/getAll-products"
+export const getSingleProduct="/product/get-product/:slug"
+export const deleteSingleProductPhoto="/product/getProduct-photo/:pid"
+export const deleteSingleProduct="/product/delete-product/:pid"
